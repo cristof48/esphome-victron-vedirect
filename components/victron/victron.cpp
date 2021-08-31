233f1dff37eb787d1d3dc595f16e68ffaf65e687
Sebastian Muszynski <s.muszynski@constructiva.de>
2021-08-31 09:23:40 +0200

    Use parse_int if used as array index

--- components/victron/victron.cpp
+++ components/victron/victron.cpp
@@ -340,7 +340,7 @@ static std::string flash_to_string(const __FlashStringHelper *flash) {
 }
 
 void VictronComponent::handle_value_() {
-  float value;
+  int value;
 
   if (label_ == "H23") {
     this->publish_state_(this->max_power_yesterday_sensor_, parse_float(value_));
@@ -365,16 +365,16 @@ void VictronComponent::handle_value_() {
   } else if (label_ == "HSDS") {
     this->publish_state_(this->day_number_sensor_, parse_float(value_));
   } else if (label_ == "CS") {
-    value = parse_float(value_);
-    this->publish_state_(this->charger_status_sensor_, value);
+    value = parse_int(value_);
+    this->publish_state_(this->charger_status_sensor_, (float) value);
     this->publish_state_(this->charger_text_sensor_, flash_to_string(charger_status_text(value)));
   } else if (label_ == "ERR") {
-    value = parse_float(value_);
-    this->publish_state_(this->error_code_sensor_, value);
+    value = parse_int(value_);
+    this->publish_state_(this->error_code_sensor_, (float) value);
     this->publish_state_(this->error_text_sensor_, flash_to_string(error_code_text(value)));
   } else if (label_ == "MPPT") {
-    value = parse_float(value_);
-    this->publish_state_(this->tracker_operation_sensor_, value);
+    value = parse_int(value_);
+    this->publish_state_(this->tracker_operation_sensor_, (float) value);
     this->publish_state_(this->tracker_text_sensor_, tracker_op_text(value));
   } else if (label_ == "FW") {
     if ((fw_version_sensor_ != nullptr) && !fw_version_sensor_->has_state())
